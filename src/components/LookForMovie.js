ebe4dc4a06cfb9b789d59b4b759eaff3f085e104
Giorgi Kintsurashvili <giokintsurashvili0@gmail.com>
2022-12-01 23:40:39 +0400

    Other filters added

--- src/components/LookForMovie.js
+++ src/components/LookForMovie.js
@@ -1,42 +1,71 @@
-import Hero from "./Hero"
+import Hero from "./Hero";
 import { useState, useEffect } from "react";
-import GenreBox from "./GenreBox";
+import ShowMovies from "./ShowMovies";
+import MultiRangeSlider from "./MultiRangeSlider";
 
-    
-const LookForMovie = () =>{
-    const[genres,setGenres] = useState([]);
-    useEffect(() => {
-        fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=02e0efe9b8e7846748af28dc0cdc3f6e&language=en-US")
-        .then(response => response.json())
-        .then(data =>{
-            setGenres(data.genres)
-            
-        })
-    })
-    var GenreSelection = genres.map((obj, i) => {
-        return(
-            <div key={i} className="genreBox form-group form-check bg-dark text-white m">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" for="exampleCheck1">{obj.name}</label>
-            </div>
-        ) 
-    })
-    return(
-        <>
-        <Hero text={"We can find a movie for you according to your interests"} />
-        <div className="LFMContainer">
-            <label className="form-label">Enter your interests</label>
-            <input type="text" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" />
-            <div id="passwordHelpBlock" className="form-text">
-                each word is taken as one key. after click space you should search another word. <br />
-                ex: drama war... <br />
-                1st word - drama. 2nd word - war.
-            </div>
-        </div>
-        <div className="genresContainer">
-        {GenreSelection}
-        </div>
-        </>
+
+const LookForMovie = () => {
+  const [genres, setGenres] = useState([]);
+  const [checked, setChecked] = useState([]);
+  var updatedYears = {};
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=02e0efe9b8e7846748af28dc0cdc3f6e&language=en-US"
     )
-}
-export default LookForMovie
+      .then((response) => response.json())
+      .then((data) => {
+        setGenres(data.genres);
+      });
+  });
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+  const handleYears = (min, max) => {
+     updatedYears = {minYear: min, maxYear: max};
+  } 
+
+  const GenreSelection = genres.map((obj, i) => {
+    
+    return (
+      <div key={i} className="genreBox form-group form-check">
+        <input
+          type="checkbox"
+          className="genreBoxInput form-check-input"
+          id={`exampleCheck${i}`}
+          value={obj.name}
+          onChange={handleCheck}
+        />
+        <label
+          className="genreBoxLabel form-check-label"
+          htmlFor={`exampleCheck${i}`}
+        >
+          {obj.name}
+        </label>
+      </div>
+    );
+  });
+
+  return (
+    <div className="LFMBody">
+      <Hero text={"We can find a movie for you according to your interests"} />
+      <form onSubmit={ShowMovies}>
+        <div className="container-fluid w-75 bg-light">
+          <h3 className="p-2">Choose Film Genres</h3>
+          <div className="genresContainer p-2">{GenreSelection}</div>
+        </div>
+        <div className="container-fluid w-75 bg-light">
+          <h3 className="p-2">Choose years interval</h3>
+          <MultiRangeSlider min={1878} max={2022} onChange={({min, max}) => handleYears({min, max})}/>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default LookForMovie;
---
+++ src/components/ShowMovies.js
@@ -0,0 +1,4 @@
+const ShowMovies =() =>{
+
+}
+export default ShowMovies
